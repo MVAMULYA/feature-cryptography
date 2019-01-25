2803f1902d06de274b9bb9493f21629be72261e2
MVAMULYA <vaishnaviamulya19@gmail.com>
2019-01-25 17:42:59 +0530

    added monoalphabet substitution cipher code

--- Ciphers.py
+++ Ciphers.py
@@ -1,15 +1,18 @@
 import string
 import random
+from copy import deepcopy
 
 Z26 = (0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25)
 small_case = string.ascii_lowercase
-upper_case = string.ascii_uppercase
+Z26copy = ''.join(random.sample(small_case,k=len(small_case)))
 small_case_dict = {}
 for i in Z26:
     small_case_dict[small_case[i]] = i
+shuffled_dict = {}
+for i in range(0,len(small_case)):
+    shuffled_dict[small_case[i]] = Z26copy[i]
 
-   
-def encryption_function(ptext,key):
+def shiftcipher_encryption(ptext,key):
     ctext = []
     for i in ptext:
         pnum = (small_case_dict[i] + key ) % 26
@@ -18,7 +21,7 @@ def encryption_function(ptext,key):
                 ctext.append(k)
     return(''.join(ctext)) 
 
-def decryption_function(ctext,key):
+def shiftcipher_decryption(ctext,key):
     ptext = []
     for i in ctext:
         cnum = (small_case_dict[i] - key) % 26
@@ -27,12 +30,36 @@ def decryption_function(ctext,key):
                 ptext.append(k)
     return(''.join(ptext))
 
-sample_text = 'testingshiftcipher'
+def monoalphabet_sub_encryption(ptext):
+    ctext = []
+    for a in ptext:
+        ctext.append(shuffled_dict[a])
+    return(''.join(ctext))
+
+def monoalphabet_sub_decryption(ctext):
+    ptext = []
+    for a in ctext:
+        for k,v in shuffled_dict.items():
+            if v == a:
+                ptext.append(k)
+    return(''.join(ptext))
+        
+
+
+sample_text = 'testingcipher'
 key = random.randint(0,25) 
-cipher_text = encryption_function(sample_text,key) 
-print(sample_text,cipher_text)
-plain_text = decryption_function(cipher_text,key)
-print(cipher_text,plain_text)
+# using shift cipher
+cipher_text = shiftcipher_encryption(sample_text,key) 
+print('encrypted shift cipher',cipher_text)
+plain_text = shiftcipher_decryption(cipher_text,key)
+print('decrypted shift text  ',plain_text)
+
+# using monoaplhabetic substitution cipher
+
+cipher_text = monoalphabet_sub_encryption(sample_text) 
+print('encrypted monoalphabet_sub cipher',cipher_text)
+plain_text = monoalphabet_sub_decryption(cipher_text)
+print('decrypted monoalphabet_sub text  ',plain_text)
 
 
 
